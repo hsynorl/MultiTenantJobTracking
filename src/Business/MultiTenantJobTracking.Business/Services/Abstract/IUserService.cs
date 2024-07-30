84b0b76159b790cc46c1e4fce4732d20b5372f2f
Targe-Huseyin <Targe-Huseyin@DESKTOP-8KQ10MV>
2024-07-30 23:46:32 +0300

    Feat(Business): Updated user service

--- src/Business/MultiTenantJobTracking.Business/Services/Abstract/IUserService.cs
+++ src/Business/MultiTenantJobTracking.Business/Services/Abstract/IUserService.cs
@@ -1,12 +1,12 @@
-﻿namespace MultiTenantJobTracking.Business.Services.Abstract
+﻿using MultiTenantJobTracking.Common.Models.User.Command;
+using MultiTenantJobTracking.Common.Models.User.ViewModel;
+
+namespace MultiTenantJobTracking.Business.Services.Abstract
 {
     public interface IUserService
     {
-        Task<bool> CreateUser();
-        Task<bool> DeleteUser();
-        Task<bool> UpdateUser();
-        Task<bool> Login();
-        Task<bool> ResetPassword();
+        Task<bool> CreateUser(CreateUserCommand createUserCommand);
+        Task<LoginViewModel> Login(LoginCommand loginCommand);
         
     }
 
--- src/Business/MultiTenantJobTracking.Business/Services/Concrete/UserService.cs
+++ src/Business/MultiTenantJobTracking.Business/Services/Concrete/UserService.cs
@@ -1,50 +1,83 @@
-﻿using MultiTenantJobTracking.Business.Services.Abstract;
+﻿using AutoMapper;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using MultiTenantJobTracking.Business.Services.Abstract;
+using MultiTenantJobTracking.Common.Models.User.Command;
+using MultiTenantJobTracking.Common.Models.User.ViewModel;
 using MultiTenantJobTracking.DataAccess.Repositories.Abstract;
+using MultiTenantJobTracking.Entities.Concrete;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace MultiTenantJobTracking.Business.Services.Concrete
 {
     public class UserService : IUserService
     {
         private readonly IUserRepository userRepository;
+        private readonly IMapper mapper;
+        readonly IConfiguration configuration;
 
-        public UserService(IUserRepository userRepository)
+        public UserService(IUserRepository userRepository, IMapper mapper, IConfiguration configuration)
         {
             this.userRepository = userRepository;
+            this.mapper = mapper;
+            this.configuration = configuration;
         }
 
-        public async Task<bool> CreateUser()
+        public async Task<bool> CreateUser(CreateUserCommand createUserCommand)
         {
-            var result = await userRepository.AddAsync(new()
-            {
-                FirstName="Test",
-                LastName="Test",
-                IsActive=true,
-                Password="Test",
-                PhoneNumber="Test",
-                EmailAddress= "Test",
-                UserType=Common.Enums.UserType.GeneralAdmin,
-            });
+            var user = mapper.Map<User>(createUserCommand);
+            var result=await userRepository.AddAsync(user);
             return result > 0;
         }
 
-        public Task<bool> DeleteUser()
+        public async Task<LoginViewModel> Login(LoginCommand loginCommand)
         {
-            throw new NotImplementedException();
-        }
+            var user=await userRepository.GetSingleAsync(p=>p.EmailAddress== loginCommand.EmailAddress);
+            if (user is null)
+            {
+                throw new Exception("Kullanıcı bulunamadı!");
+            }
+            if (user.Password!=loginCommand.Password)
+            {
+                throw new Exception("Hatalı Şifre!");
+            }
+            LoginViewModel loginViewModel = new LoginViewModel();   
+           
+            var claims = new Claim[]
+          {
+            new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+            new Claim(ClaimTypes.Email, user.EmailAddress),
+            new Claim(ClaimTypes.GivenName, user.FirstName),
+            new Claim(ClaimTypes.Surname, user.LastName),
+            new Claim(ClaimTypes.Role,user.UserType.ToString())
+          };
 
-        public Task<bool> Login()
-        {
-            throw new NotImplementedException();
-        }
+            var token = GenerateToken(claims);
 
-        public Task<bool> ResetPassword()
-        {
-            throw new NotImplementedException();
-        }
+            loginViewModel.Token = token.Item1;
+            loginViewModel.TokenExpire = token.Item2;
 
-        public Task<bool> UpdateUser()
+            return loginViewModel;
+
+
+        }
+        private (string, DateTime) GenerateToken(Claim[] claims)
         {
-            throw new NotImplementedException();
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["AuthConfig:Secret"]));
+
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+            var expiryDate = DateTime.Now.AddDays(10);
+            var expiry = new DateTime(expiryDate.Year, expiryDate.Month, expiryDate.Day, expiryDate.Hour, expiryDate.Minute, expiryDate.Second);
+
+
+            var token = new JwtSecurityToken(claims: claims,
+                                             expires: expiry,
+                                             signingCredentials: creds
+                                             );
+
+            return (new JwtSecurityTokenHandler().WriteToken(token), expiry);
         }
     }
 }
