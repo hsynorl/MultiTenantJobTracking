5f29e71e42e0b63b087c4d9e11a313aee03bc37d
Targe-Huseyin <Targe-Huseyin@DESKTOP-8KQ10MV>
2024-07-31 11:05:39 +0300

    Feat(Api): Added department user controller

---
+++ src/WebApi/MultiTenantJobTracking.WebApi/Controllers/DepartmentUsersController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MultiTenantJobTracking.Business.Services.Abstract;
+using MultiTenantJobTracking.Common.Models.DepartmentUser.Command;
+
+namespace MultiTenantJobTracking.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DepartmentUsersController : ControllerBase
+    {
+        private readonly IDepartmentUserService departmentUserService;
+
+        public DepartmentUsersController(IDepartmentUserService departmentUserService)
+        {
+            this.departmentUserService = departmentUserService;
+        }
+
+        [HttpPost("create-department-user")]
+        public async Task<IActionResult> CreateDepartmentUser(CreateDepartmentUserCommand createDepartmentUserCommand)
+        {
+            var result=await departmentUserService.CreateDepartmentUser(createDepartmentUserCommand);
+            return Ok(result);
+        }
+        
+        [HttpGet("get-department-user")]
+        public async Task<IActionResult> GetDepartmentUser([FromQuery] Guid UserId)
+        {
+            var result = await departmentUserService.GetDepartmentUsers(UserId);
+            return Ok(result);
+        }
+
+    }
+}
